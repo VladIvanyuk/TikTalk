1e4f27ed09d15d62be6eded1ff2727772e2abc78
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2025-08-24 19:00:17 +1000

    feat: integrate reactive forms in login component for improved validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,43 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+type LoginFormGroup = {
+  username: FormControl<string>;
+  password: FormControl<string>;
+};
 
 @Component({
   selector: 'app-login',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+  form: FormGroup<LoginFormGroup>;
+
+  constructor() {
+    this.form = this.fb.group<LoginFormGroup>({
+      username: this.fb.nonNullable.control('', [Validators.required]),
+      password: this.fb.nonNullable.control('', [Validators.required]),
+    });
+  }
+
   readonly isPasswordVisible = signal(false);
 
   togglePasswordVisibility(): void {
     this.isPasswordVisible.set(!this.isPasswordVisible());
+  }
+
+  onSubmit(): void {
+    console.log(this.form.getRawValue());
   }
 }
