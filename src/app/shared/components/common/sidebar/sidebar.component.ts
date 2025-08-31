58a142ceb18a4fad78a035ac146baae18e1df416
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2025-08-31 16:46:48 +1000

    feat: refactor sidebar component to utilize custom SVG icon component for improved maintainability

--- src/app/shared/components/common/sidebar/sidebar.component.ts
+++ src/app/shared/components/common/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, SvgIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
