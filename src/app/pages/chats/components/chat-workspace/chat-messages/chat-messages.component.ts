63e9df56c89616938c09ce8e455bdd50bfe7981e
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2026-03-23 08:10:44 +1000

    feat: implement message grouping by date in chat workspace, enhance message display with date separators, and update styles for improved readability

--- src/app/pages/chats/components/chat-workspace/chat-messages/chat-messages.component.ts
+++ src/app/pages/chats/components/chat-workspace/chat-messages/chat-messages.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { Chat } from '../../../../../shared/services/chat/model/types';
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { Chat, ChatMessage } from '../../../../../shared/services/chat/model/types';
 import { ProfileService } from '../../../../../shared/services/profile/profile.service';
 import { AvatarComponent } from '../../../../../shared/components/common/avatar/avatar.component';
 import { avatarSizes } from '../../../../../shared/types/components.types';
@@ -17,4 +17,19 @@ export class ChatMessagesComponent {
   readonly myProfile = inject(ProfileService).myProfile;
 
   readonly avatarSizes = avatarSizes;
+
+  readonly filterredByDateMessages = computed(() => {
+    const messagesWithDate: { date: string; messages: ChatMessage[] }[] = [];
+
+    this.chat().messages.forEach((message) => {
+      const date = new Date(message.createdAt).toISOString().split('T')[0];
+      if (!messagesWithDate.find((item) => item.date === date)) {
+        messagesWithDate.push({ date, messages: [message] });
+      } else {
+        messagesWithDate.find((item) => item.date === date)?.messages.push(message);
+      }
+    });
+
+    return messagesWithDate;
+  });
 }
